052dc4b3ff4f0d5572738f6acc1b45037b8948f8
dream_Faunus <cappu159@gmail.com>
2021-05-26 15:03:52 +0900

    User Model 구현

--- app/src/routes/home/home.ctrl.js
+++ app/src/routes/home/home.ctrl.js
@@ -1,7 +1,6 @@
-"use strict";
 // MVC 중 c = controller section
-
-const UserStorage = require("../../models/UserStorage");
+"use strict";
+const User = require("../../models/User");
 
 const output = {
   home: (req, res) => {
@@ -15,23 +14,9 @@ const output = {
 
 const process = {
   login: (req, res) => {
-    const id = req.body.id;
-    const pw = req.body.pw;
-
-    const users = UserStorage.getUsers("id", "pw");
-
-    const response = {};
-
-    if (users.id.includes(id)) {
-      const idx = users.id.indexOf(id);
-      if (users.pw[idx] === pw) {
-        response.success = true;
-        return res.json(response);
-      }
-    }
-
-    response.success = false;
-    response.msg = "로그인에 실패!!!";
+    const user = new User(req.body);
+    const response = user.login();
+    //console.log(response);
     return res.json(response);
   },
 };

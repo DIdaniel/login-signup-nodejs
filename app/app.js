26ca7f206167f0f751d70eafb24f0461fc455286
dream_Faunus <cappu159@gmail.com>
2021-05-26 10:14:01 +0900

    frontend create javascript file

--- app/app.js
+++ app/app.js
@@ -10,6 +10,7 @@ const home = require("./src/routes/home");
 // App setting
 app.set("views", "./src/views");
 app.set("view engine", "ejs");
+app.use(express.static(`${__dirname}/src/public`));
 
 app.use("/", home); // use => middleware를 연결(등록)해주는 메서드
 
